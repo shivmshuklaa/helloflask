cc2807f89cf4fb19b2300551420ac5a6fbc1ff8b
shivam <shivams9296@hotmail.com>
2023-01-08 04:22:49 +0530

    adding create_app

--- index.py
+++ index.py
@@ -1,10 +1,5 @@
-from flask import Flask
+from app import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route("/")
-def index():
-    return "Hello Flask!"
-
-if __name__ == "__main__":
-    app.run()
+app.run()
